e10132d347ef9368b5dee2366b98ccd616413756
Auster Chen <auster.s.chen@gmail.com>
2014-10-28 11:41:42 -0700

    Cat controller complete

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -5,10 +5,10 @@ class Cat < ActiveRecord::Base
   validates :color, presence: true, inclusion: { in: COLOR }
   validates :birth_date, presence: true
   validates :name, presence: true
-  validate on_or_before_today
-  
+  validate :on_or_before_today
+
   def on_or_before_today
-    if self.birth_date > Date.today
+    if !self.birth_date.nil? && self.birth_date > Date.today 
       errors[:birth_date] << "is invalid"
     end
   end
