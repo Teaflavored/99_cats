0d1f1da218e92a7c27237ad5f945d1f8b3b74eec
Auster Chen <auster.s.chen@gmail.com>
2014-10-28 10:34:58 -0700

    Cat model complete

---
+++ app/models/cat.rb
@@ -0,0 +1,19 @@
+class Cat < ActiveRecord::Base
+  SEX = ["M", "F"]
+  COLOR = ["black", "brown", "gray", "white", "chocolate", "calico", "tortie"]
+  validates :sex, presence: true, inclusion: { in: SEX }
+  validates :color, presence: true, inclusion: { in: COLOR }
+  validates :birth_date, presence: true
+  validates :name, presence: true
+  validate on_or_before_today
+  
+  def on_or_before_today
+    if self.birth_date > Date.today
+      errors[:birth_date] << "is invalid"
+    end
+  end
+  
+  def age
+    (Date.today - self.birth_date).numerator/365
+  end
+end
