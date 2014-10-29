999204657954bc0865f17c1a65323e40cdaa5b25
Auster Chen <auster.s.chen@gmail.com>
2014-10-28 18:02:18 -0700

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cat.transaction do
+  30.times do 
+    Cat.create(name: Faker::Name.name, color: "black", sex: "M",
+    birth_date: Faker::Date.backward(10000), description: Faker::Hacker.say_something_smart)
+  end
+end
+
+CatRentalRequest.transaction do
+  cat_ids = Cat.all.pluck(:id)
+  250.times do
+    CatRentalRequest.create(cat_id: cat_ids.sample, start_date: Faker::Date.between(Date.today, Date.new(2015,1,1)), end_date: Faker::Date.between(Date.new(2015,11,11), Date.new(2016,1,1)))
+  end
+end
