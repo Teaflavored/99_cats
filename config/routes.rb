48d9bffbe6df60ed58ce767b1b3eda3537e992c8
Auster Chen <Auster.s.chen@gmail.com>
2014-10-29 22:08:09 -0700

    more css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cats#index'
+  get '/learn_more', to: "static_pages#about", as: "about"
   get 'cats/requests_cats', to: 'cats#request_cats', as: 'cat_request_cats'
   resources :cats do
   	resources :cat_rental_requests, only: [:new, :create]
